b19a32757cafc2d5f9672272543e8a24ffda24b2
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-01-28 16:54:40 +0100

    Added env variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    cohereKey: "Dcy3MwPSur2Q1RsWrcfnx2LrrZzgmrUcy1rL9Nqm",
+    cohereGenerateUrl: "https://api.cohere.ai/generate"
+  }
 }
 
 module.exports = nextConfig
