671046fa1b894951193fa942386ebb9f817d6f62
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-01-28 19:07:41 +0100

    Changed sidebar bg color

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -29,7 +29,7 @@ const Sidebar = () => {
         className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full lg:translate-x-0`}
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-800">
+        <div className="h-full px-3 py-4 overflow-y-auto bg-slate-800">
           <ul className="space-y-2">
             <li>
               <a
