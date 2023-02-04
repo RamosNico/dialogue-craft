6c1485e51e73111214a3224b195fdde9399af647
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-02-04 13:40:23 +0100

    Implemented states for useCohere

--- services/useCohere.ts
+++ services/useCohere.ts
@@ -1,12 +1,16 @@
 import cohere from "cohere-ai";
-
-cohere.init(process.env.cohereKey as string);
+import { useState } from "react";
 
 const useCohere = (input: {} = {}) => {
-  const data = {
+  cohere.init(process.env.cohereKey as string);
+  const [data, setData] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const body = {
     model: "command-xlarge-20221108",
     prompt:
-      "We have two persons:\n- Nicolas, age 26, an arrogant and romantic man\n- Melisa, age 24, a sensitive and talkative woman\n\nWrite me a conversation between these two persons for a movie script in the style where they are a couple discussing what they are having for dinner, he calls her honey and she calls him sweetie. Where they end up deciding to eat hot dogs.",
+      "We have two persons:\n- Nicolas, age 26, an arrogant and romantic man\n- Melisa, age 24, a sensitive and talkative woman\nWrite me a conversation between these two persons for a movie script in the style where they are a couple discussing what they are having for dinner, he calls her honey and she calls him sweetie. Where they end up deciding to eat hot dogs.",
     max_tokens: 416,
     temperature: 1.2,
     k: 0,
@@ -18,20 +22,31 @@ const useCohere = (input: {} = {}) => {
   };
 
   const fetchData = async () => {
-    const response = await fetch(process.env.cohereGenerateUrl as string, {
-      method: "POST",
-      headers: {
-        Authorization: `BEARER ${process.env.cohereKey}`,
-        "Content-Type": "application/json",
-        "Cohere-Version": "2022-12-06",
-      },
-      body: JSON.stringify(data),
-    }).then((res) => res.json());
+    setIsLoading(true);
+    setData("");
+    setError("");
+    try {
+      const response = await fetch(process.env.cohereGenerateUrl as string, {
+        method: "POST",
+        headers: {
+          Authorization: `BEARER ${process.env.cohereKey}`,
+          "Content-Type": "application/json",
+          "Cohere-Version": "2022-12-06",
+        },
+        body: JSON.stringify(body),
+      });
+      if (!response.ok) throw new Error("Something went wrong with the API");
+      const result = await response.json();
 
-    return console.log(`Prediction: ${response.generations[0].text}`);
+      setIsLoading(false);
+      setData(result.generations[0].text.trim());
+    } catch (err) {
+      setIsLoading(false);
+      setError(err as string);
+    }
   };
 
-  return { fetchData };
+  return { data, error, isLoading, fetchData };
 };
 
 export default useCohere;
