e70e1f556a2f0bd2ecfb076938e11d1a76478e4d
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-02-04 19:19:29 +0100

    Removed some useless sidebar link and changed the Home icon

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Sidebar = () => {
   return (
     <nav>
@@ -10,8 +12,8 @@ const Sidebar = () => {
       >
         <span className="sr-only">Open sidebar</span>
         <svg
+          aria-hidden
           className="w-6 h-6"
-          aria-hidden="true"
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
@@ -32,71 +34,35 @@ const Sidebar = () => {
         <div className="h-full px-3 py-4 overflow-y-auto bg-slate-800">
           <ul className="space-y-2">
             <li>
-              <a
-                href="#"
+              <Link
                 className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg hover:bg-slate-700 transition-all"
+                href="/"
               >
                 <svg
-                  aria-hidden="true"
-                  className="w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-100 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
-                  <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
-                </svg>
-                <span className="ml-3">Dashboard</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg hover:bg-slate-700 transition-all"
-              >
-                <svg
-                  aria-hidden="true"
+                  aria-hidden
                   className="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-100 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2.0"
+                  viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  ></path>
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
-                <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
-                  Pro
-                </span>
-              </a>
+                <span className="ml-3">Home</span>
+              </Link>
             </li>
             <li>
-              <a
-                href="#"
+              <Link
+                href="/characters"
                 className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg hover:bg-slate-700 transition-all"
               >
                 <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-100 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
-                  <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
-                </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Inbox</span>
-                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">
-                  3
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg hover:bg-slate-700 transition-all"
-              >
-                <svg
-                  aria-hidden="true"
+                  aria-hidden
                   className="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-100 dark:group-hover:text-white"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -108,8 +74,10 @@ const Sidebar = () => {
                     clipRule="evenodd"
                   ></path>
                 </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Users</span>
-              </a>
+                <span className="flex-1 ml-3 whitespace-nowrap">
+                  Characters
+                </span>
+              </Link>
             </li>
             <li>
               <a
@@ -117,28 +85,7 @@ const Sidebar = () => {
                 className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg hover:bg-slate-700 transition-all"
               >
                 <svg
-                  aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-100 dark:group-hover:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="flex-1 ml-3 whitespace-nowrap">Products</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg hover:bg-slate-700 transition-all"
-              >
-                <svg
-                  aria-hidden="true"
+                  aria-hidden
                   className="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-100 dark:group-hover:text-white"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -159,7 +106,7 @@ const Sidebar = () => {
                 className="flex items-center p-2 text-base font-normal text-gray-100 rounded-lg hover:bg-slate-700 transition-all"
               >
                 <svg
-                  aria-hidden="true"
+                  aria-hidden
                   className="flex-shrink-0 w-6 h-6 text-gray-100 transition duration-75 group-hover:text-gray-100 dark:group-hover:text-white"
                   fill="currentColor"
                   viewBox="0 0 20 20"
@@ -179,6 +126,6 @@ const Sidebar = () => {
       </aside>
     </nav>
   );
-}
+};
 
 export default Sidebar;
