667bc823475ebd6aae98f1bd08b291cbba1a0b42
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-02-04 19:19:13 +0100

    Moved the characters code to it's own page

---
+++ pages/characters/index.tsx
@@ -0,0 +1,53 @@
+import useCharactersList from "@/helpers/useCharactersList";
+import { Button } from "flowbite-react";
+
+export default function CharactersPage() {
+  const { charList, addChar, removeChar } = useCharactersList();
+
+  const newChar = {
+    name: "Rodrigo",
+    age: 24,
+    description:
+      "A man who just came to this world from an outer one and does not remember anything.",
+  };
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold">Characters</h1>
+      <p>Here's a list with your current characters:</p>
+
+      <ul>
+        {charList.map((char) => (
+          <li key={char.id} className="flex gap-2">
+            <p>{`${char.name} (ID ${char.id})`}</p>
+            <svg
+              className="w-6 h-6 cursor-pointer hover:stroke-red-600 transition-all"
+              onClick={() => removeChar(char.id)}
+              fill="none"
+              stroke="currentColor"
+              strokeWidth={1.5}
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </li>
+        ))}
+      </ul>
+
+      <div className="flex gap-6">
+        <Button
+          className="w-full bg-blue-700 hover:bg-blue-600 disabled:bg-blue-700 disabled:hover:bg-blue-700 transition-all"
+          onClick={() => addChar({ id: charList[charList.length - 1].id + 1, ...newChar })}
+        >
+          Add new character
+        </Button>
+      </div>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import useCohere from "@/services/useCohere";
 import { Button, Spinner } from "flowbite-react";
 
 export default function Home() {
-  const { charList, addChar, removeChar } = useCharactersList();
+  const { charList } = useCharactersList();
   const { data, isLoading, error, fetchData } = useCohere();
   if (error) console.error(error);
 
@@ -25,33 +25,6 @@ export default function Home() {
     <>
       <h1 className="text-5xl font-bold">Get a new dialog</h1>
       <p>Here you will input your characters and description</p>
-      <p>Here's a list with your current characters:</p>
-      <ul>
-        {charList.map((char) => (
-          <li>{`${char.name} (ID ${char.id})`}</li>
-        ))}
-      </ul>
-
-      <div className="flex gap-6">
-        <Button
-          className="w-full bg-red-700 hover:bg-red-600 disabled:bg-red-700 disabled:hover:bg-red-700 transition-all"
-          onClick={() => removeChar(0)}
-        >
-          Remove Julia
-        </Button>
-        <Button
-          className="w-full bg-red-700 hover:bg-red-600 disabled:bg-red-700 disabled:hover:bg-red-700 transition-all"
-          onClick={() => removeChar(1)}
-        >
-          Remove Alfonso
-        </Button>
-        <Button
-          className="w-full bg-blue-700 hover:bg-blue-600 disabled:bg-blue-700 disabled:hover:bg-blue-700 transition-all"
-          onClick={() => addChar({id: charList[charList.length - 1].id + 1, ...newChar})}
-        >
-          Add new character
-        </Button>
-      </div>
 
       <Button
         className="bg-cyan-700 hover:bg-cyan-600 disabled:bg-cyan-700 disabled:hover:bg-cyan-700 transition-all"
