bba009b6d31df3153e9d4da23dd1fb6317974ce1
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-02-04 19:19:49 +0100

    Removed unused flowbite stylesheet and added the global one

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="My project for the co:here hackathon" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
+        <link href="../styles/globals.css" />
       </Head>
 
       <Sidebar />
