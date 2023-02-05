34e578374c7646026361a592b113fc4680f2e7aa
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-02-05 21:16:25 +0100

    refactored useCohere to work with a prompt got from props

--- services/useCohere.ts
+++ services/useCohere.ts
@@ -2,27 +2,26 @@ import cohere from "cohere-ai";
 import { useState } from "react";
 import axios from "axios";
 
-const useCohere = (input: {} = {}) => {
+const useCohere = () => {
   cohere.init(process.env.cohereKey as string);
   const [data, setData] = useState("");
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const body = {
-    model: "command-xlarge-20221108",
-    prompt:
-      "We have two persons:\n- Nicolas, age 26, an arrogant and romantic man\n- Melisa, age 24, a sensitive and talkative woman\nWrite me a conversation between these two persons for a movie script in the style where they are a couple discussing what they are having for dinner, he calls her honey and she calls him sweetie. Where they end up deciding to eat hot dogs.",
-    max_tokens: 416,
-    temperature: 1.1,
-    k: 0,
-    p: 0.75,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-    stop_sequences: [],
-    return_likelihoods: "NONE",
-  };
+  const fetchData = (prompt: string) => {
+    const body = {
+      model: "command-xlarge-20221108",
+      prompt: prompt,
+      max_tokens: 416,
+      temperature: 1.1,
+      k: 0,
+      p: 0.75,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      stop_sequences: [],
+      return_likelihoods: "NONE",
+    };
 
-  const fetchData = () => {
     setIsLoading(true);
     setData("");
     setError("");
