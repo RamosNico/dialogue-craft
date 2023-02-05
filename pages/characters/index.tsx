a647e09ea1853462c2a15fb462c2fa7d0ac21a67
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-02-05 22:34:21 +0100

    Changes

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,13 +1,13 @@
 import CharactersTable from "@/components/characters/characters-table";
-import useCharactersList from "@/helpers/useCharactersList";
+import useCharacters from "@/services/useCharacters";
 import Character from "@/models/character";
 import { Button, Label } from "flowbite-react";
 import { useState } from "react";
 
-interface NewChar extends Omit<Character, "id"> {};
+interface NewChar extends Omit<Character, "id"> {}
 
 export default function CharactersPage() {
-  const { charList, addChar, removeChar } = useCharactersList();
+  const { charList, addChar, removeChar } = useCharacters();
   const [newChar, setNewChar] = useState<NewChar>({
     name: "",
     age: undefined,
@@ -23,70 +23,78 @@ export default function CharactersPage() {
 
   const handleForm = (e: React.FormEvent) => {
     e.preventDefault();
-    addChar({...newChar});
+    addChar({ ...newChar });
   };
 
   return (
     <>
       <h1 className="mb-6 text-5xl font-bold">Characters</h1>
-      <p className="mb-4">Here's a list with your current characters:</p>
-      <CharactersTable charList={charList} removeChar={removeChar} />
+      <section>
+        <p className="mb-4">Here's a list with your current characters:</p>
+        <CharactersTable charList={charList} removeChar={removeChar} />
+      </section>
 
-      <h2 className="mt-6 mb-3 text-3xl font-bold">Add new character</h2>
-      <form onSubmit={(e) => handleForm(e)}>
-        <div className="flex justify-between gap-8">
-          <div className="mb-5 w-full">
+      <section>
+        <h2 className="mt-6 mb-3 text-3xl font-bold">Add new character</h2>
+        <form onSubmit={(e) => handleForm(e)}>
+          <div className="flex justify-between gap-8">
+            <div className="mb-5 w-full">
+              <Label className="text-gray-100 text-base font-normal">
+                Character's name
+              </Label>
+              <input
+                className="mt-1 bg-gray-700 border placeholder-gray-400 text-gray-100 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5"
+                type="text"
+                id="charName"
+                placeholder={charPlaceholder.name}
+                value={newChar?.name}
+                onChange={(e) =>
+                  setNewChar({ ...newChar, name: e.target.value })
+                }
+                required
+              />
+            </div>
+
+            <div className="mb-5 w-full">
+              <Label className="text-gray-100 text-base font-normal">
+                Character's age
+              </Label>
+              <input
+                className="mt-1 bg-gray-700 border placeholder-gray-400 text-gray-100 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5"
+                type="text"
+                id="charAge"
+                placeholder={charPlaceholder.age.toString()}
+                value={newChar?.age}
+                onChange={(e) => setNewChar({ ...newChar, age: +e.target.value })}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="mb-5">
             <Label className="text-gray-100 text-base font-normal">
-              Character's name
+              Character's description
             </Label>
-            <input
-              className="mt-1 bg-gray-700 border placeholder-gray-400 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              type="text"
-              id="charName"
-              placeholder={charPlaceholder.name}
-              value={newChar?.name}
-              onChange={(e) => setNewChar({...newChar, name: e.target.value})}
+            <textarea
+              className="mt-1 bg-gray-700 border placeholder-gray-400 text-gray-100 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5"
+              id="charDesc"
+              placeholder={charPlaceholder.description}
+              value={newChar?.description}
+              onChange={(e) =>
+                setNewChar({ ...newChar, description: e.target.value })
+              }
+              rows={3}
               required
             />
           </div>
-
-          <div className="mb-5 w-full">
-            <Label className="text-gray-100 text-base font-normal">
-              Character's age
-            </Label>
-            <input
-              className="mt-1 bg-gray-700 border placeholder-gray-400 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-              type="text"
-              id="charAge"
-              placeholder={charPlaceholder.age.toString()}
-              value={newChar?.age}
-              onChange={(e) => setNewChar({...newChar, age: +e.target.value})}
-              required
-            />
-          </div>
-        </div>
-
-        <div className="mb-5">
-          <Label className="text-gray-100 text-base font-normal">
-            Character's description
-          </Label>
-          <textarea
-            className="mt-1 bg-gray-700 border placeholder-gray-400 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-            id="charDesc"
-            placeholder={charPlaceholder.description}
-            value={newChar?.description}
-            onChange={(e) => setNewChar({...newChar, description: e.target.value})}
-            rows={3}
-            required
-          />
-        </div>
-        <Button
-          className="mt-8 w-full bg-cyan-700 hover:bg-cyan-600 disabled:bg-cyan-700 disabled:hover:bg-cyan-700 transition-all"
-          type="submit"
-        >
-          <span className="text-base">Add new character</span>
-        </Button>
-      </form>
+          <Button
+            className="mt-8 w-full bg-cyan-700 hover:bg-cyan-600 disabled:bg-cyan-700 disabled:hover:bg-cyan-700 transition-all"
+            type="submit"
+          >
+            <span className="text-base">Add new character</span>
+          </Button>
+        </form>
+      </section>
     </>
   );
 }
