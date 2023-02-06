80862150d780f16c24c6deedfd14f475c76c4bc7
Nicolas Ramos <nicolas@eurowelcome.co.uk>
2023-02-06 19:54:31 +0100

    Added dev credits at the end of the Sidebar

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -80,7 +80,11 @@ const Sidebar = () => {
               </Link>
             </li>
           </ul>
+          <Link className="absolute bottom-3 text-gray-200 hover:text-cyan-500 transition-all" href="https://nicolas-ramos.com" target="_blank">
+            <p className="ml-2 text-sm text-center">Developed by Nicolas Ramos</p>
+          </Link>
         </div>
+
       </aside>
     </nav>
   );
